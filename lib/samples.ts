c97db7ad66cfa0a3b5f3d42b0f066d14ab52e509
Adam Ramsis <adam.ramsis.2@gmail.com>
2026-03-19 21:08:17 +0100

    feat: add street1, street2, interieur1, interieur2 sample images to demo

--- lib/samples.ts
+++ lib/samples.ts
@@ -10,4 +10,8 @@ export const SAMPLES: Sample[] = [
   { id: "portrait", label: "Portrait", src: "/samples/portrait.jpg" },
   { id: "landscape", label: "Landscape", src: "/samples/landscape.jpg" },
   { id: "object", label: "Object", src: "/samples/object.jpg" },
+  { id: "street1", label: "Street 1", src: "/samples/street1.png" },
+  { id: "street2", label: "Street 2", src: "/samples/street2.png" },
+  { id: "interieur1", label: "Intérieur 1", src: "/samples/interieur1.png" },
+  { id: "interieur2", label: "Intérieur 2", src: "/samples/interieur2.png" },
 ];
