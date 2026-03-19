5a360cba0effd781d99a8d245e3d64d24b895359
Adam Ramsis <adam.ramsis.2@gmail.com>
2026-03-19 12:39:27 +0100

    pdf adjustments

--- lib/team.ts
+++ lib/team.ts
@@ -9,19 +9,19 @@ export const TEAM: TeamMember[] = [
   {
     name: "Pierlouis Pillet",
     role: "ML Engineer",
-    github: "https://github.com/TODO-pierlouis",
+    github: "https://github.com/pl-plt",
     linkedin: "https://linkedin.com/in/TODO-pierlouis",
   },
   {
     name: "Adam Ramsis",
     role: "ML Engineer",
-    github: "https://github.com/aramsis",
+    github: "https://github.com/aramz33",
     linkedin: "https://linkedin.com/in/TODO-adam",
   },
   {
     name: "Rodrick Zegang",
     role: "ML Engineer",
-    github: "https://github.com/TODO-rodrick",
+    github: "https://github.com/rodrickAurellZegang",
     linkedin: "https://linkedin.com/in/TODO-rodrick",
   },
 ];
